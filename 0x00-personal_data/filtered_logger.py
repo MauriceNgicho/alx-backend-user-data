6f3ecf3acaf48a28036d705c826c53051ba013f9
Maurice Ngicho <mrcngicho@gmail.com>
2025-05-25 02:33:45 +0300

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,8 @@
 import logging
 import re
 from typing import List
+import os
+import mysql.connector
 
 
 # Constants
@@ -55,3 +57,18 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db():
+    """Returns a connection to the MySQL database using env variables."""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+            user=username,
+            password=password,
+            host=host,
+            database=db_name
+    )
