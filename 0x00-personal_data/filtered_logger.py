b76e8d28b8553d179e911db1787301a691c592c6
Maurice Ngicho <mrcngicho@gmail.com>
2025-05-25 01:14:12 +0300

    Logg formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Module for filtering PII from log messages"""
 
 
+import logging
 import re
 from typing import List
 
@@ -15,3 +16,23 @@ def filter_datum(
             rf'({"|".join(fields)})=.*?(?={separator})',
             lambda m: f'{m.group(1)}={redaction}', message
             )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class for logging sensitive information """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters sensitive fields using filter_datum"""
+        record.msg = filter_datum(
+                self.fields, self.REDACTION, record.getMessage(),
+                self.SEPARATOR
+                )
+        return super().format(record)
