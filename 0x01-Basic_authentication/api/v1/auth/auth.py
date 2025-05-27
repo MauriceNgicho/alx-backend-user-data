f197280937d3c0b3460e4427665c9f404f1b54a1
Maurice Ngicho <mrcngicho@gmail.com>
2025-05-27 17:11:13 +0300

    Auth class to handle authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Auth module to manage API authentication"""
+
+
+from flask import requests
+from typing import List, TypeVar
+
+
+class Auth:
+    """A class to manage authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Return False"""
+        return (False)
+
+    def authorization_header(self, request=None) -> List[str]:
+    """Returns None"""
+        return (None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Return None"""
+        return (None)
