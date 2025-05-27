8e711b6acd7725cd65f6ce427b5b6d5e00e64bef
Maurice Ngicho <mrcngicho@gmail.com>
2025-05-28 00:24:10 +0300

    Deffine routes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,16 @@ class Auth:
     """A class to manage authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Return False"""
-        return (False)
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        for excluded in excluded_paths:
+            if excluded == path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> List[str]:
         """Returns None"""
